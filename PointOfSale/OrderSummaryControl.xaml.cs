56427ba548026c6b16ebfec1b8b1ad9fd588400a
unknown <islater@ksu.edu>
2020-03-29 19:47:41 -0500

    asdf

--- PointOfSale/OrderSummaryControl.xaml.cs
+++ PointOfSale/OrderSummaryControl.xaml.cs
@@ -31,7 +31,11 @@ namespace PointOfSale
             
         }
 
-
+        /// <summary>
+        /// finds the current item and remvoes it from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void RemoveItemClicked(object sender, RoutedEventArgs e)
         {
             
@@ -44,12 +48,21 @@ namespace PointOfSale
 
 
         }
-
-        private void ItemName_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        /// <summary>
+        /// pulls up the customization screen of the selected item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ItemName_PreviewMouseDown(object sender, MouseEventArgs e)
         {
+
             
-            IOrderItem i = (IOrderItem)((TextBlock)e.Source).DataContext;
-            var a = this.Parent;
+            
+            MainWindow a = (MainWindow)((Grid)this.Parent).Parent;
+            if (((TextBlock)e.Source).DataContext is IOrderItem item)
+            {
+                a.ordercontrol.MainWindow.Child = new CustomizationControl(item);
+            }
         }
     }
 }
